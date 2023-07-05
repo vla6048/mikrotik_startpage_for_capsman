dbae68c10f0de76424f048adf4a976bebd6ce3bc
vla6048 <vla6048@gmail.com>
2023-07-05 15:09:36 +0300

    add sqlalchemy. Query to freeradius database

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-import ipaddress
 import os
 import socket
 import struct
 from flask import Flask, request, render_template, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
 import routeros_api
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.getenv('SP_USER')}:{os.getenv('SP_PASSWD')}@{os.getenv('SP_HOST')}/radius"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-def conver_ip(ip):
-    print(ip)
+class RadCheck(db.Model):
+    __tablename__ = 'radcheck'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(64), nullable=False)
+    attribute = db.Column(db.String(64), nullable=False)
+    op = db.Column(db.String(2), nullable=False, default='==')
+    value = db.Column(db.String(253), nullable=False)
+
+# вот тут нужно расписать создание двух таблиц, userbillinfo и userinfo, для корректного отображения в daloradius
+# class UserBillInfo(db.Model):
+#     __tablename__ = 'userbillinfo'
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     username = db.Column(db.String(128), nullable=True)
+#
+
+
+def convert_ip(ip):
     converted_ip = socket.inet_ntoa(struct.pack('!L', int(ip)))
-    print(converted_ip)
     return converted_ip
 
 
@@ -24,8 +41,6 @@ def perform_action(mac_addr, mikrotik_ip):
         return 'Ошибка: Не заданы переменные среды для подключения к MikroTik'
 
     try:
-        print(mac_addr)
-        print(mikrotik_ip)
         connection = routeros_api.RouterOsApiPool(host=mikrotik_host,
                                                   username=mikrotik_username,
                                                   password=mikrotik_password,
@@ -37,23 +52,19 @@ def perform_action(mac_addr, mikrotik_ip):
         connection.disconnect()
         return '+'
     except Exception as e:
-        print(e)
         return 'Ошибка при выполнении действия: {}'.format(str(e))
 
 
-# Маршрут для главной страницы
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
-# Маршрут для страницы API
 @app.route('/api/action-get', methods=['GET'])
 def api_get():
     mac_address = request.args.get('mac')
     mikrotik_ip_str = request.args.get('mikrotik_ip')
     mikrotik_ip = mikrotik_ip_str
-    print(mac_address, mikrotik_ip)
     return render_template('mktapi.html', mac_address=mac_address, mikrotik_ip=mikrotik_ip)
 
 
@@ -61,11 +72,16 @@ def api_get():
 def api_post():
     mac_address = request.form.get('mac_address')
     mikrotik_ip_numeric = request.form.get('mikrotik_ip')
-    mikrotik_ip = conver_ip(mikrotik_ip_numeric)
+    mikrotik_ip = convert_ip(mikrotik_ip_numeric)
 
     result = perform_action(mac_address, mikrotik_ip)
 
     if result == '+':
+        # Добавление записи в таблицу radcheck
+        radcheck_entry = RadCheck(username=mac_address, attribute='Cleartext-Password', op=':=', value='1')
+        db.session.add(radcheck_entry)
+        db.session.commit()
+
         return render_template('finish.html')
     else:
         return result
